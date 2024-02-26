ef00ce429f2548592d6cd27ac0c734ea9ff09f1e
Fortox <ori6g1@gmail.com>
2024-02-26 17:09:30 +0200

    GameObject and Position
    added the start of gameobject and position classes

---
+++ Final_Project/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum ObjectType
+{
+    enemy,
+    player,
+    npc,
+    terrain,
+    projectile,
+    item,
+    door
+}
+
+internal class GameObject
+{
+    ObjectType Type;
+    Position Position;
+
+    public GameObject(ObjectType type, Position position) 
+    { 
+        Type = type;
+        Position = position;
+    }
+}
---
+++ Final_Project/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Position
+{
+    public int x, y;
+
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void ChangePosition(Position newPosition)
+    {
+        this.x = newPosition.x;
+        this.y = newPosition.y;
+    }
+}
+
